16e37e41a577cd5ebee52a910b3a646df45bb08c
Brad Fontaine <brad.fontaine@enbridge.com>
2019-04-03 14:51:39 -0600

    Authenticating with Okta, logout is next

--- addon/authenticators/okta.js
+++ addon/authenticators/okta.js
@@ -1,14 +1,30 @@
 import Base from 'ember-simple-auth/authenticators/base';
+import OktaAuth from '@okta/okta-auth-js';
 
 export default Base.extend({
+
+  _client: undefined,
+
+  init(){
+    this._super(...arguments);
+    this._client = new OktaAuth({
+      url: '',
+    });
+  },
+
   restore(data) {
   },
 
   authenticate(username, password) {
-    console.info('Credentials', username, password);
-
+    return this._client
+      .signIn({
+        username,
+        password
+      });
   },
 
-  invalidate(data) {
+  invalidate(/*data*/) {
+    return this._client
+      .signOut();
   }
 });
--- tests/dummy/app/controllers/docs/demos/index.js
+++ tests/dummy/app/controllers/docs/demos/index.js
@@ -1,15 +1,22 @@
 import Controller from '@ember/controller';
-import { inject as service } from '@ember-decorators/service';
-import { action } from '@ember-decorators/object';
+import {inject as service} from '@ember-decorators/service';
+import {action} from '@ember-decorators/object';
+
 
 export default class DocsDemoIndexController extends Controller {
   @service session;
-  username = 'Sterling';
+  username = '';
   password = '';
 
-  @action login(username, password){
+  @action login(username, password) {
     console.info('Trying to login!', this.session.isAuthenticated);
     this.session.authenticate('authenticator:okta', username, password);
+    return false;
+  }
+
+  @action logout(){
+    this.session.invalidate();
+    console.info('Invalidating Session', this.session.isAuthenticated);
     return false;
   }
 }
