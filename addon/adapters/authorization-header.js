e898a2e234243a609410e5e782119e8f568ed7c5
Dan Nelson <dan@cybertooth.io>
2019-04-09 16:25:29 -0600

    #3 configuration will include `headerAuthorization` key.  New adapter for including the Authorization-Bearer for all data requests.

--- addon/adapters/authorization-header.js
+++ addon/adapters/authorization-header.js
@@ -1,9 +1,10 @@
 import { inject as service } from '@ember-decorators/service';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 import DS from 'ember-data';
 
 const { Adapter } = DS;
 
-export default class AuthorizationHeaderAdapter extends Adapter {
+export default class AuthorizationHeaderAdapter extends Adapter.extend(DataAdapterMixin) {
   @service configuration;
   @service session;
 
